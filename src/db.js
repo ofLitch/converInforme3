207180943581424314cc77596e24ee61f37cd8ed
ofLitch <davidalejo004@hotmail.com>
2025-03-10 04:13:51 -0500

    Subiendo proyecto a coverInforme3

--- src/db.js
+++ src/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-
-
-const DB_USER = "admin"; // Usuario de MongoDB
-const DB_PASSWORD = "vagrant"; // Contraseña del usuario
-const DB_HOST = "selene.unicauca.edu.co"; // O IP del servidor MongoDB
-const DB_PORT = "8004"; // Puerto en el que corre MongoDB
-const DB_NAME = "appdb"; // Nombre de la base de datos
+const DB_USER = "admin"; 
+const DB_PASSWORD = "password123"; 
+const DB_HOST = "192.168.1.15"; 
+const DB_PORT = "27017"; 
+const DB_NAME = "test"; 
 
 const mongoURI = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}?authSource=admin`;
+
 export const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI);
@@ -17,7 +16,4 @@ export const connectDB = async () => {
     } catch (error) {
         console.log(error);
     }
-    
 }
-
-
